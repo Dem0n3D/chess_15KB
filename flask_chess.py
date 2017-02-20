5888a1966f6f3a8c64e5effbbfba91dc90ab24b5
Дмитрий Баранов <baranov@semograph.com>
2017-02-20 12:55:53 +0500

    Реализовано сохранение партий в БД. Настроен alembic.

--- flask_chess.py
+++ flask_chess.py
@@ -2,59 +2,41 @@ from flask import Flask, render_template, redirect, url_for
 
 from alchemy import db
 
+from models.chess import *
+
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://chess1:qwerty@localhost:5432/chess1'
+app.config['SQLALCHEMY_ECHO'] = True
+
 db.init_app(app)
 
-colors = ['black', 'white']
-rows = [0, 7]
-
-figures = []
-
-for c in range(2):
-    for i in range(8):
-        figures.append(
-            {"name": 'pawn', "text": '♟' if c == 1 else '♙', "color": colors[c], "row": 1 if c == 0 else 6,
-             "col": i})
-
-        figures += [
-            {"name": 'rook', "text": '♜' if c == 1 else '♖', "color": colors[c], "row": rows[c], "col": 0},
-            {"name": 'knight', "text": '♞' if c == 1 else '♘', "color": colors[c], "row": rows[c], "col": 1},
-            {"name": 'bishop', "text": '♝' if c == 1 else '♗', "color": colors[c], "row": rows[c], "col": 2},
-            {"name": 'rook', "text": '♜' if c == 1 else '♖', "color": colors[c], "row": rows[c], "col": 7},
-            {"name": 'knight', "text": '♞' if c == 1 else '♘', "color": colors[c], "row": rows[c], "col": 6},
-            {"name": 'bishop', "text": '♝' if c == 1 else '♗', "color": colors[c], "row": rows[c], "col": 5},
-            {"name": 'king', "text": '♚' if c == 1 else '♔', "color": colors[c], "row": rows[c], "col": 4},
-            {"name": 'queen', "text": '♛' if c == 1 else '♕', "color": colors[c], "row": rows[c], "col": 3},
-        ]
-
-
-def find_figure(i, j):
-    try:
-        return [f for f in figures if f["row"] == i and f["col"] == j][0]
-    except IndexError:
-        return None
-
-
-board = [[find_figure(i, j) for j in range(8)] for i in range(8)]
-
-
 @app.route('/')
-@app.route('/board')
-def board1():
-    return render_template("board.html", **{"rows": range(8), "cols": range(8), "board": board})
+@app.route('/board/<int:id>')
+def board1(id):
+    game = db.session.query(Game).get(id)
 
-@app.route('/board/<start>')
-def turn1(start):
-    return render_template("board.html", **{"rows": range(8), "cols": range(8), "board": board, "start": start})
+    if(not game):
+        game = Game(id=id)
+        db.session.add(game)
+        db.session.commit()
 
-@app.route('/board/<start>/<end>')
-def turn2(start, end):
-    board[int(end[0])][int(end[1])] = board[int(start[0])][int(start[1])]
-    board[int(start[0])][int(start[1])] = None
-    return redirect(url_for("board1"))
+    return render_template("board.html", **{"id": id, "rows": range(8), "cols": range(8), "board": game.board()})
+
+@app.route('/board/<int:id>/<start>')
+def turn1(id, start):
+    game = db.session.query(Game).get(id)
+
+    return render_template("board.html", **{"id": id, "rows": range(8), "cols": range(8), "board": game.board(), "start": start})
+
+@app.route('/board/<int:id>/<start>/<end>')
+def turn2(id, start, end):
+    game = db.session.query(Game).get(id)
+
+    game.move(start, end)
+
+    return redirect(url_for("board1", id=id))
 
 if __name__ == '__main__':
     app.run()
--- models/chess.py
+++ models/chess.py
@@ -1,4 +1,5 @@
 from sqlalchemy import *
+from sqlalchemy.orm import relationship, backref
 
 from alchemy import db
 
@@ -9,6 +10,10 @@ class Turn(db.Model):
 
     src = Column(CHAR(2), nullable=False)
     dst = Column(CHAR(2), nullable=False)
+
+    game_id = Column(Integer, ForeignKey("game.id"), nullable=False)
+    game = relationship("Game", backref=backref("turns", order_by=id))
+
 
 class Figure(db.Model):
     id = Column(Integer, primary_key=True)
@@ -30,3 +35,50 @@ class Figure(db.Model):
             "♛": "queen",
             "♟": "pawn"
         }[self.symbol]
+
+
+class Game(db.Model):
+
+    id = Column(Integer, primary_key=True)
+
+    def move(self, start, end):
+        turn = Turn(src=start, dst=end, game=self)
+        db.session.add(turn)
+        db.session.commit()
+
+    def board(self):
+        colors = ['black', 'white']
+        rows = [0, 7]
+
+        figures = []
+
+        for c in range(2):
+            for i in range(8):
+                figures.append(
+                    {"name": 'pawn', "text": '♟' if c == 1 else '♙', "color": colors[c], "row": 1 if c == 0 else 6,
+                     "col": i})
+
+                figures += [
+                    {"name": 'rook', "text": '♜' if c == 1 else '♖', "color": colors[c], "row": rows[c], "col": 0},
+                    {"name": 'knight', "text": '♞' if c == 1 else '♘', "color": colors[c], "row": rows[c], "col": 1},
+                    {"name": 'bishop', "text": '♝' if c == 1 else '♗', "color": colors[c], "row": rows[c], "col": 2},
+                    {"name": 'rook', "text": '♜' if c == 1 else '♖', "color": colors[c], "row": rows[c], "col": 7},
+                    {"name": 'knight', "text": '♞' if c == 1 else '♘', "color": colors[c], "row": rows[c], "col": 6},
+                    {"name": 'bishop', "text": '♝' if c == 1 else '♗', "color": colors[c], "row": rows[c], "col": 5},
+                    {"name": 'king', "text": '♚' if c == 1 else '♔', "color": colors[c], "row": rows[c], "col": 4},
+                    {"name": 'queen', "text": '♛' if c == 1 else '♕', "color": colors[c], "row": rows[c], "col": 3},
+                ]
+
+        def find_figure(i, j):
+            try:
+                return [f for f in figures if f["row"] == i and f["col"] == j][0]
+            except IndexError:
+                return None
+
+        board = [[find_figure(i, j) for j in range(8)] for i in range(8)]
+
+        for turn in self.turns:
+            board[int(turn.dst[0])][int(turn.dst[1])] = board[int(turn.src[0])][int(turn.src[1])]
+            board[int(turn.src[0])][int(turn.src[1])] = None
+
+        return board
