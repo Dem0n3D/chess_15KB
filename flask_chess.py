52620200a33b9046392367ec1862f1c3780ad0d0
Дмитрий Баранов <baranov@semograph.com>
2017-10-06 09:55:22 +0500

    Модульное тестирование серверной части.

--- flask_chess.py
+++ flask_chess.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, jsonify
+from flask import Flask, render_template, redirect, url_for, jsonify, Blueprint
 
 from alchemy import db
 
@@ -7,18 +7,21 @@ from models.chess import *
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://chess1:qwerty@localhost:5432/chess1'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://chess:qwerty@localhost:15435/chess'
 app.config['SQLALCHEMY_ECHO'] = True
 
 db.init_app(app)
 
 
-@app.route('/<int:id>')
-@app.route('/board/<int:id>')
+bp = Blueprint("chess", __name__)
+
+
+@bp.route('/<int:id>')
+@bp.route('/board/<int:id>')
 def board(id):
     game = db.session.query(Game).get(id)
 
-    if (not game):
+    if not game:
         game = Game(id=id)
         db.session.add(game)
         db.session.commit()
@@ -26,7 +29,7 @@ def board(id):
     return render_template("board.html", id=id)
 
 
-@app.route('/board/<int:id>/figures')
+@bp.route('/board/<int:id>/figures')
 def board1(id):
     game = db.session.query(Game).get(id)
 
@@ -37,13 +40,16 @@ def board1(id):
         "moves": moves
     })
 
-@app.route('/board/<int:id>/<start>/<end>', methods=['POST'])
+@bp.route('/board/<int:id>/<start>/<end>', methods=['POST'])
 def turn(id, start, end):
     game = db.session.query(Game).get(id)
 
     game.move(start, end)
 
-    return redirect(url_for("board1", id=id))
+    return redirect(url_for("chess.board1", id=id))
+
+
+app.register_blueprint(bp)
 
 if __name__ == '__main__':
     app.run()
---
+++ tests/chess.py
@@ -0,0 +1,69 @@
+# -*- coding: utf_8 -*-
+import json
+import unittest
+
+import chess
+from flask import Flask
+from sqlalchemy import func
+
+from alchemy import db
+from flask_chess import board, board1, bp
+from models.chess import Game
+
+
+class TestChess(unittest.TestCase):
+
+    def setUp(self):
+        self.app = Flask(__name__, template_folder="../templates")
+        self.app.config["DEBUG"] = True
+        self.app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://chess:qwerty@localhost:15435/chess'
+        self.app.config['SQLALCHEMY_ECHO'] = True
+        self.app.register_blueprint(bp)
+
+        db.init_app(self.app)
+
+        self.app.testing = True
+        self.app.app_context().push()
+
+        self.client = self.app.test_client()
+
+        db.session.begin_nested()
+
+    def tearDown(self):
+        db.session.rollback()
+
+    def test_board(self):
+        id = db.session.query(func.max(Game.id))[0][0] + 1
+        r1 = self.client.get("/board/"+str(id))
+        r2 = self.client.get("/board/"+str(id))
+        self.assertEquals(r1.data.decode("utf-8"), r2.data.decode("utf-8"))
+
+    def test_board1(self):
+        id = db.session.query(func.max(Game.id))[0][0] + 1
+        self.client.get("/board/"+str(id))
+        r = self.client.get("/board/{}/figures".format(id))
+        self.assertEquals(r.status_code, 200)
+
+        moves = [[m.uci()[0:2], m.uci()[2:]] for m in chess.Board().legal_moves]
+        data = json.loads(r.data.decode("utf-8"))
+
+        game = db.session.query(Game).get(id)
+
+        self.assertEquals(data["moves"], moves)
+        self.assertEquals(data["board"], game.board())
+
+    def test_turn(self):
+        id = db.session.query(func.max(Game.id))[0][0] + 1
+        self.client.get("/board/"+str(id))
+        r = self.client.post("/board/{}/{}/{}".format(id, "e2", "e4"), follow_redirects=True)
+        self.assertEquals(r.status_code, 200)
+
+        board = chess.Board()
+        board.push(chess.Move.from_uci("e2e4"))
+        moves = [[m.uci()[0:2], m.uci()[2:]] for m in board.legal_moves]
+        data = json.loads(r.data.decode("utf-8"))
+
+        game = db.session.query(Game).get(id)
+
+        self.assertEquals(data["moves"], moves)
+        self.assertEquals(data["board"], game.board())
